b4ccd0cb9d5e1dabca9952321902decb8feec883
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-06 19:28:33 +0300

    added first boxes and images

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@
 import './App.css';
 //components
 import Header from './components/Header';
-
 //images
 import orangeMain from './assets/images/orange_blue_main.jpg'
+import bananaYellow from './assets/images/banana_yellow.jpg'
+import cupPink from './assets/images/cup_pink.jpg'
+import papayaGreen from './assets/images/papaya_green.jpg'
+import avocadoPink from './assets/images/avocado_pink.jpg'
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <section className='mainContainer'>
         <div className="box box1">
+          <Header />
           <img src={orangeMain} alt="orange" />
+        </div>
+        <div className="otherBoxes">
+          <div className="box boxSecondary textBox box2">
+            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, maxime quo. Eum voluptate, cumque soluta numquam cum error consequatur praesentium!</p>
+            <a href="#" className="boxLink">Learn More</a>
+          </div>
+          <div className="box boxSecondary box3">
+          </div>
+          <div className="box boxSecondary box4">
+          </div>
+          <div className="box boxSecondary textBox box5">
+            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates et quasi repellendus omnis qui minus possimus illum, id praesentium tenetur.</p>
+            <a href="#" className="boxLink">Learn More</a>
+          </div>
+          <div className="box boxSecondary box6">
+          </div>
+          <div className="box boxSecondary box7">
+          </div>
         </div>
       </section>
     </div>
