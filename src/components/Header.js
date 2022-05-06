9dfdbd5d50f370b367d65548647e155b0c29b05d
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-06 16:45:47 +0300

    created header and navbar components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+//components
+import Navbar from "./Navbar";
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>sunnyside &#9728;</h1>
+            <Navbar />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <ul className="navbar">
+            <li className="navLink activeLink">Home</li>
+            <li className="navLink">About</li>
+            <li className="navLink">Projects</li>
+            <li className="navLink">Contact</li>
+        </ul>
+    );
+}
+
+export default Navbar;
